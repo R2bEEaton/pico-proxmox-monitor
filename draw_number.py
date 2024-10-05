fcfe924dcf6b12bbea7be55c1d1c4da6a59aaefa
Ryan Eaton <scgryan@gmail.com>
2024-10-05 13:30:46 -0500

    Create draw_number.py

---
+++ draw_number.py
@@ -0,0 +1,160 @@
+def d0(h75, x, y, r, g, b):
+    h75.set_pixel(x + 0, y + 0, r, g, b)
+    h75.set_pixel(x + 1, y + 0, r, g, b)
+    h75.set_pixel(x + 2, y + 0, r, g, b)
+    h75.set_pixel(x + 0, y + 1, r, g, b)
+    h75.set_pixel(x + 2, y + 1, r, g, b)
+    h75.set_pixel(x + 0, y + 2, r, g, b)
+    h75.set_pixel(x + 2, y + 2, r, g, b)
+    h75.set_pixel(x + 0, y + 3, r, g, b)
+    h75.set_pixel(x + 2, y + 3, r, g, b)
+    h75.set_pixel(x + 0, y + 4, r, g, b)
+    h75.set_pixel(x + 1, y + 4, r, g, b)
+    h75.set_pixel(x + 2, y + 4, r, g, b)
+    return 3
+
+def d1(h75, x, y, r, g, b):
+    h75.set_pixel(x + 0, y + 0, r, g, b)
+    h75.set_pixel(x + 0, y + 1, r, g, b)
+    h75.set_pixel(x + 0, y + 2, r, g, b)
+    h75.set_pixel(x + 0, y + 3, r, g, b)
+    h75.set_pixel(x + 0, y + 4, r, g, b)
+    return 1
+    
+def d2(h75, x, y, r, g, b):
+    h75.set_pixel(x + 0, y + 0, r, g, b)
+    h75.set_pixel(x + 1, y + 0, r, g, b)
+    h75.set_pixel(x + 2, y + 0, r, g, b)
+    h75.set_pixel(x + 2, y + 1, r, g, b)
+    h75.set_pixel(x + 0, y + 2, r, g, b)
+    h75.set_pixel(x + 1, y + 2, r, g, b)
+    h75.set_pixel(x + 2, y + 2, r, g, b)
+    h75.set_pixel(x + 0, y + 3, r, g, b)
+    h75.set_pixel(x + 0, y + 4, r, g, b)
+    h75.set_pixel(x + 1, y + 4, r, g, b)
+    h75.set_pixel(x + 2, y + 4, r, g, b)
+    return 3
+    
+def d3(h75, x, y, r, g, b):
+    h75.set_pixel(x + 0, y + 0, r, g, b)
+    h75.set_pixel(x + 1, y + 0, r, g, b)
+    h75.set_pixel(x + 2, y + 0, r, g, b)
+    h75.set_pixel(x + 2, y + 1, r, g, b)
+    h75.set_pixel(x + 0, y + 2, r, g, b)
+    h75.set_pixel(x + 1, y + 2, r, g, b)
+    h75.set_pixel(x + 2, y + 2, r, g, b)
+    h75.set_pixel(x + 2, y + 3, r, g, b)
+    h75.set_pixel(x + 0, y + 4, r, g, b)
+    h75.set_pixel(x + 1, y + 4, r, g, b)
+    h75.set_pixel(x + 2, y + 4, r, g, b)
+    return 3
+    
+def d4(h75, x, y, r, g, b):
+    h75.set_pixel(x + 0, y + 0, r, g, b)
+    h75.set_pixel(x + 2, y + 0, r, g, b)
+    h75.set_pixel(x + 0, y + 1, r, g, b)
+    h75.set_pixel(x + 2, y + 1, r, g, b)
+    h75.set_pixel(x + 0, y + 2, r, g, b)
+    h75.set_pixel(x + 1, y + 2, r, g, b)
+    h75.set_pixel(x + 2, y + 2, r, g, b)
+    h75.set_pixel(x + 2, y + 3, r, g, b)
+    h75.set_pixel(x + 2, y + 4, r, g, b)
+    return 3
+    
+def d5(h75, x, y, r, g, b):
+    h75.set_pixel(x + 0, y + 0, r, g, b)
+    h75.set_pixel(x + 1, y + 0, r, g, b)
+    h75.set_pixel(x + 2, y + 0, r, g, b)
+    h75.set_pixel(x + 0, y + 1, r, g, b)
+    h75.set_pixel(x + 0, y + 2, r, g, b)
+    h75.set_pixel(x + 1, y + 2, r, g, b)
+    h75.set_pixel(x + 2, y + 2, r, g, b)
+    h75.set_pixel(x + 2, y + 3, r, g, b)
+    h75.set_pixel(x + 0, y + 4, r, g, b)
+    h75.set_pixel(x + 1, y + 4, r, g, b)
+    h75.set_pixel(x + 2, y + 4, r, g, b)
+    return 3
+    
+def d6(h75, x, y, r, g, b):
+    h75.set_pixel(x + 0, y + 0, r, g, b)
+    h75.set_pixel(x + 1, y + 0, r, g, b)
+    h75.set_pixel(x + 2, y + 0, r, g, b)
+    h75.set_pixel(x + 0, y + 1, r, g, b)
+    h75.set_pixel(x + 0, y + 2, r, g, b)
+    h75.set_pixel(x + 1, y + 2, r, g, b)
+    h75.set_pixel(x + 2, y + 2, r, g, b)
+    h75.set_pixel(x + 0, y + 3, r, g, b)
+    h75.set_pixel(x + 2, y + 3, r, g, b)
+    h75.set_pixel(x + 0, y + 4, r, g, b)
+    h75.set_pixel(x + 1, y + 4, r, g, b)
+    h75.set_pixel(x + 2, y + 4, r, g, b)
+    return 3
+    
+def d7(h75, x, y, r, g, b):
+    h75.set_pixel(x + 0, y + 0, r, g, b)
+    h75.set_pixel(x + 1, y + 0, r, g, b)
+    h75.set_pixel(x + 2, y + 0, r, g, b)
+    h75.set_pixel(x + 2, y + 1, r, g, b)
+    h75.set_pixel(x + 2, y + 2, r, g, b)
+    h75.set_pixel(x + 2, y + 3, r, g, b)
+    h75.set_pixel(x + 2, y + 4, r, g, b)
+    return 3
+    
+def d8(h75, x, y, r, g, b):
+    h75.set_pixel(x + 0, y + 0, r, g, b)
+    h75.set_pixel(x + 1, y + 0, r, g, b)
+    h75.set_pixel(x + 2, y + 0, r, g, b)
+    h75.set_pixel(x + 0, y + 1, r, g, b)
+    h75.set_pixel(x + 2, y + 1, r, g, b)
+    h75.set_pixel(x + 0, y + 2, r, g, b)
+    h75.set_pixel(x + 1, y + 2, r, g, b)
+    h75.set_pixel(x + 2, y + 2, r, g, b)
+    h75.set_pixel(x + 0, y + 3, r, g, b)
+    h75.set_pixel(x + 2, y + 3, r, g, b)
+    h75.set_pixel(x + 0, y + 4, r, g, b)
+    h75.set_pixel(x + 1, y + 4, r, g, b)
+    h75.set_pixel(x + 2, y + 4, r, g, b)
+    return 3
+    
+def d9(h75, x, y, r, g, b):
+    h75.set_pixel(x + 0, y + 0, r, g, b)
+    h75.set_pixel(x + 1, y + 0, r, g, b)
+    h75.set_pixel(x + 2, y + 0, r, g, b)
+    h75.set_pixel(x + 0, y + 1, r, g, b)
+    h75.set_pixel(x + 2, y + 1, r, g, b)
+    h75.set_pixel(x + 0, y + 2, r, g, b)
+    h75.set_pixel(x + 1, y + 2, r, g, b)
+    h75.set_pixel(x + 2, y + 2, r, g, b)
+    h75.set_pixel(x + 2, y + 3, r, g, b)
+    h75.set_pixel(x + 0, y + 4, r, g, b)
+    h75.set_pixel(x + 1, y + 4, r, g, b)
+    h75.set_pixel(x + 2, y + 4, r, g, b)
+    return 3
+
+def dd(h75, x, y, r, g, b):
+    h75.set_pixel(x + 0, y + 4, r, g, b)
+    return 1
+
+
+draw_funcs = [d0, d1, d2, d3, d4, d5, d6, d7, d8, d9, dd]
+
+
+def draw_number(h75, num, x, y, r, g, b):
+    num = str(num)
+    for i in range(len(num)):
+        c = num[i]
+        if c in "0123456789.":
+            w = draw_funcs["0123456789.".index(c)](h75, x, y, r, g, b)
+            x += w + 1
+            
+if __name__ == "__main__":
+    import hub75
+    
+    HEIGHT = 32
+    WIDTH = 64
+
+    h75 = hub75.Hub75(WIDTH, HEIGHT, stb_invert=False)
+    h75.start()
+    
+    draw_number(h75, 1.234567890, 0, 0, 255, 255, 255)
+
